2606aca8ef6a7a931423e04db814ed84e4d2d505
Chance Callahan <chance@chancecallahan.com>
2023-07-20 23:09:10 -0400

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://chief.placenl.nl/ws';
+export const CHIEF_WS_ENDPOINT = ___WS_ENDPOINT___ || 'wss://placestartchief.rabbitcomputing.com/ws';
 export const USERSCRIPT_REVISION = ___GIT_HASH___ || 'development';
