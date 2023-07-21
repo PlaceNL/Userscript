c88fa6d17103d452d6ef590d1f7473cde33edffe
Noah van der Aa <ndvdaa@gmail.com>
2023-07-22 00:51:11 +0200

    Fix priority image loading

--- src/ws/handler/orders.js
+++ src/ws/handler/orders.js
@@ -8,8 +8,11 @@ export async function handleOrder(client, payload) {
     client.orderReference.clearRect(0, 0, client.orderReference.canvas.width, client.orderReference.canvas.height);
     client.orderPriority.clearRect(0, 0, client.orderPriority.canvas.width, client.orderPriority.canvas.height);
 
+    let x = 1500 + payload.offset.x;
+    let y = 1000 + payload.offset.y;
+
     await Promise.all([
-        loadURLToCanvas(client.orderReference, payload.images.order, 1500 + payload.offset.x, 1000 + payload.offset.y),
-        payload.images.priority ? loadURLToCanvas(client.orderPriority, payload.images.priority) : Promise.resolve() // an empty promise - a lie
+        loadURLToCanvas(client.orderReference, payload.images.order, x, y),
+        payload.images.priority ? loadURLToCanvas(client.orderPriority, payload.images.priority, x, y) : Promise.resolve() // an empty promise - a lie
     ]);
 }
